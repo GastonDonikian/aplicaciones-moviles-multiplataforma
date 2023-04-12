eaa0898b0c21339c75dce720fee62afbe0fff6fc
JSicardi <jsicardi@itba.edu.ar>
2023-04-11 22:16:31 -0300

    Molecules/Atoms: Removed background from images and added upper logo to App Bar

--- my_app/lib/design_system/atoms/logo.dart
+++ my_app/lib/design_system/atoms/logo.dart
@@ -1,40 +1,26 @@
 import 'package:flutter/material.dart';
 
 class SerManosSquareLogo extends StatelessWidget {
-  final double widthImage;
-
-  const SerManosSquareLogo({
-    Key? key,
-    required this.widthImage,
-  }) : super(key: key);
+  const SerManosSquareLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Widget logoImage = Image.asset(
-      'assets/square_logo.jpeg',
-      width: widthImage,
-      height: widthImage,
+      'assets/square_logo.png',
     );
     return logoImage;
   }
 }
 
 class SerManosRectangleLogo extends StatelessWidget {
-  final double widthImage;
-  final double heightImage;
-
   const SerManosRectangleLogo({
     Key? key,
-    required this.widthImage,
-    required this.heightImage,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Widget logoImage = Image.asset(
-      'assets/rectangular_logo.jpeg',
-      width: widthImage,
-      height: heightImage,
+      'assets/rectangular_logo.png',
     );
     return logoImage;
   }
--- my_app/lib/design_system/cells/app_bar.dart
+++ my_app/lib/design_system/cells/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/design_system/atoms/logo.dart';
 import 'package:my_app/design_system/cells/tab_bar.dart';
 import 'package:my_app/design_system/foundations/colors.dart';
 
@@ -8,6 +9,7 @@ class SerManosAppBar extends AppBar {
     super.key,
     required this.customTabBar,
   }) : super(
+            title: const SerManosRectangleLogo(),
             backgroundColor: SerManosColorFoundations.statusBarColor,
             bottom: PreferredSize(
               preferredSize: customTabBar.preferredSize,
