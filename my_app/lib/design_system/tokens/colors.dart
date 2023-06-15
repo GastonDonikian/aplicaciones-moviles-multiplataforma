c675ae2033d0b3c446ca7edab4333ede5d849330
inesmarca <imarcarian@itba.edu.ar>
2023-06-15 16:07:24 -0300

    Tokens: added updated colors

--- my_app/lib/design_system/tokens/colors.dart
+++ my_app/lib/design_system/tokens/colors.dart
@@ -5,15 +5,19 @@ class SerManosColors {
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF191919);
-  static const Color grey10 = Color(0xFFFAFAFA);
   static const Color blackOpacity15 = Color(0x26000000);
   static const Color blackOpacity30 = Color(0x4D000000);
+  static const Color grey10 = Color(0xFFFAFAFA);
   static const Color grey25 = Color(0xFFE0E0E0);
   static const Color grey50 = Color(0xFF9E9E9E);
   static const Color grey75 = Color(0xFF666666);
 
-  static const Color primary = Color(0xFF14903F);
+  static const Color primary5 = Color(0xFFF3F9F5);
+  static const Color primary10 = Color(0xFFE7F4EC);
+  static const Color primary100 = Color(0xFF14903F);
 
+  static const Color secondary10 = Color(0xFFE1ECF5);
+  static const Color secondary25 = Color(0xFFCAE5FB);
   static const Color secondary50 = Color(0XFF90CAF9);
   static const Color secondary80 = Color(0XFF6CB6F3);
   static const Color secondary90 = Color(0XFF4AA9F5);
