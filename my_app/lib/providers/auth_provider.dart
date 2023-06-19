ebfacbd079670703ad2e7bb3415e66ac8c81e1b9
inesmarca <imarcarian@itba.edu.ar>
2023-06-19 11:13:24 -0300

    Fixed auth provider for navigation

--- my_app/lib/providers/auth_provider.dart
+++ my_app/lib/providers/auth_provider.dart
@@ -1,6 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_app/providers/user_provider.dart';
 
-final authProvider = StreamProvider<User?>((ref) {
-  return FirebaseAuth.instance.authStateChanges();
+class AuthNotifier extends StateNotifier<bool> {
+  Ref ref;
+  AuthNotifier(this.ref) : super(false) {
+    ref.listen(userProvider, (previousState, nextState) {
+      if (previousState == null && nextState != null) {
+        state = true;
+      } else if (previousState != null && nextState == null) {
+        state = false;
+      }
+    });
+  }
+}
+
+final authProvider = StateNotifierProvider<AuthNotifier, bool>((ref) {
+  return AuthNotifier(ref);
 });
--- my_app/lib/services/navigation_service.dart
+++ my_app/lib/services/navigation_service.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_app/models/news.dart';
-import 'package:my_app/models/volunteer.dart';
 import 'package:my_app/models/volunteer_association.dart';
 import 'package:my_app/pages/edit_profile.dart';
 import 'package:my_app/pages/home.dart';
@@ -12,7 +11,7 @@ import 'package:my_app/pages/news_details.dart';
 import 'package:my_app/pages/signup.dart';
 import 'package:my_app/pages/volunteer_association.dart';
 import 'package:my_app/pages/welcome.dart';
-import 'package:my_app/providers/user_provider.dart';
+import 'package:my_app/providers/auth_provider.dart';
 import 'package:my_app/screens/apply.dart';
 import 'package:my_app/screens/news.dart';
 import 'package:my_app/screens/profile.dart';
@@ -21,7 +20,7 @@ final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
 final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>();
 
 final routerProvider = Provider<GoRouter>((ref) {
-  final Volunteer? user = ref.watch(userProvider);
+  final bool isLogged = ref.watch(authProvider);
 
   return GoRouter(
     navigatorKey: _rootNavigatorKey,
@@ -113,15 +112,13 @@ final routerProvider = Provider<GoRouter>((ref) {
       ),
     ],
     redirect: (context, state) {
-      final isAuth = user != null;
-
       final needsAuth = state.location != LoginPage.routeLocation &&
           state.location != SignUpPage.routeLocation &&
           state.location != LandingPage.routeLocation;
 
-      if (needsAuth) return isAuth ? null : LandingPage.routeLocation;
+      if (needsAuth) return isLogged ? null : LandingPage.routeLocation;
 
-      if (!needsAuth && isAuth) return ApplyTab.routeLocation;
+      if (!needsAuth && isLogged) return ApplyTab.routeLocation;
 
       return null;
     },
